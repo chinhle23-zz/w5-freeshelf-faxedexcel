53bcfc98d40cb7b2106cc2563e36ca6e651f618b
faxedexcel <chinh.le23@gmail.com>
2019-03-14 13:36:02 -0400

    "testing heroku"

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,5 +154,5 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     # Logs any emails sent to the console (so the password reset link can be copied from the console for testing)
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
     # locals returns a dictionary of all your local variables
--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('accounts/', include('registration.backends.default.urls')),
         # URL patterns for the views in django-registration-redux
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # allows us to use CSS files in the static directory
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # allows us to use uploaded images
